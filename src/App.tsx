e545dbec4205bc60ccab65ad5e1474a8568140f7
Tereshkov Sergei[D] <rat87@mail.ru>
2021-12-19 20:40:42 +0300

    add: task 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
-import React from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import ShopItemFunc from "./components/ShopItemFunc"
 
 const App = () => {
+  const item = {
+    brand: "Tiger of Sweden",
+    title: "Leonard coat",
+    description: "Minimalistic coat in cotton-blend",
+    descriptionFull:
+      "Men's minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.",
+    price: 399,
+    currency: "£",
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="background-element" />
+      <div className="highlight-window">
+        <div className="highlight-overlay" />
+      </div>
+      <div className="window">
+        <ShopItemFunc item={item} />
+      </div>
     </div>
   )
 }
---
+++ src/components/ShopItemFunc/ShopItemFunc.tsx
@@ -0,0 +1,31 @@
+type IProps = {
+  item: {
+    brand: string
+    title: string
+    description: string
+    descriptionFull: string
+    price: number
+    currency: string
+  }
+}
+
+const ShopItemFunc = ({ item }: IProps) => {
+  return (
+    <div className="main-content">
+      <h2>{item.brand}</h2>
+      <h1>{item.title}</h1>
+      <h3>{item.description}</h3>
+      <div className="description">{item.descriptionFull}</div>
+      <div className="highlight-window mobile">
+        <div className="highlight-overlay" />
+      </div>
+      <div className="divider" />
+      <div className="purchase-info">
+        <div className="price">£399.00</div>
+        <button type="button">Добавить в корзину</button>
+      </div>
+    </div>
+  )
+}
+
+export default ShopItemFunc
