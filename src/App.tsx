6f835cac24fe39b53e0998283dbe22c2b8b3c6b4
Sergei Tereshkov <s.tereshkov@alarmtrade.ru>
2021-12-21 10:32:29 +0300

    refactor: App
       -   move common interface to App
       -   refactor and style taskSwitcher

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,48 @@ import Calendar from "./components/Calendar"
 import ShopItemClass from "./components/ShopItemClass"
 import ShopItemFunc from "./components/ShopItemFunc"
 
+export interface IItem {
+  item: {
+    brand: string
+    title: string
+    description: string
+    descriptionFull: string
+    price: number
+    currency: string
+  }
+}
+
 const App = () => {
-  const [task, setTask] = useState("2")
+  const [task, setTask] = useState("1")
+
+  const handler = (e: any) => {
+    setTask(e.target.value)
+  }
+
+  const tasksSwitcher = (
+    <div
+      style={{
+        position: "absolute",
+        zIndex: 9,
+        top: 0,
+        left: 0,
+        background: "rgba(255,255,255,0.8)",
+        padding: "10px",
+        margin: "5px",
+        border: "1px solid black",
+        borderRadius: "30%",
+      }}
+    >
+      <input type="radio" id="contactChoice1" name="contact" value="1" defaultChecked={task == "1"} onChange={handler} />
+      <label htmlFor="contactChoice1">Task1 (ShopItemFunc)</label>
+      <br />
+      <input type="radio" id="contactChoice2" name="contact" value="2" defaultChecked={task == "2"} onChange={handler} />
+      <label htmlFor="contactChoice2">Task2 (ShopItemClass)</label>
+      <br />
+      <input type="radio" id="contactChoice3" name="contact" value="3" defaultChecked={task == "3"} onChange={handler} />
+      <label htmlFor="contactChoice3">Task3 (Calendar)</label>
+    </div>
+  )
 
   const item = {
     brand: "Tiger of Sweden",
@@ -17,20 +57,9 @@ const App = () => {
     currency: "£",
   }
 
-  const handler = (e: any) => {
-    setTask(e.target.value)
-  }
-
   return (
     <>
-      <div style={{ position: "absolute", zIndex: 9, top: 0, left: 0 }}>
-        <input type="radio" id="contactChoice1" name="contact" value="1" defaultChecked onChange={handler} />
-        <label htmlFor="contactChoice1">Task1</label>
-        <input type="radio" id="contactChoice2" name="contact" value="2" onChange={handler} />
-        <label htmlFor="contactChoice2">Task2</label>
-        <input type="radio" id="contactChoice3" name="contact" value="3" onChange={handler} />
-        <label htmlFor="contactChoice3">Task3</label>
-      </div>
+      {tasksSwitcher}
 
       {task == "1" && (
         <div className="container">
